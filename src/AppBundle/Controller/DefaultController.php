57035db55dd09f6739775b63aca081d40f954bbc
ellecito <haterelle@gmail.com>
2017-11-29 23:10:47 -0300

    Session & Bower

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Usuario;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class DefaultController extends Controller
 {
@@ -13,10 +15,27 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'title' => 'Animal',
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        if($request->getMethod() === "POST"){
+            $repository = $this->getDoctrine()->getRepository(Usuario::class);
+            $usuario = $repository->findOneBy(
+                array('email' => $request->request->get('email'), 'password' => md5($request->request->get('password')))
+            );
+
+            if (!$usuario) {
+                throw $this->createNotFoundException(
+                    'Error al iniciar sesion'
+                );
+            }else{
+                $session = $request->getSession();
+                $session->start();
+                $session->set('usuario', $usuario);          
+            }
+        
+        }else{
+            return $this->render('default/index.html.twig', [
+                'title' => 'Login',
+                'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            ]);
+        }
     }
 }
